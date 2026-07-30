b14597d371bba4742545797980ce461476cdd1a0
rebamalho <153083946+rebamalho@users.noreply.github.com>
2026-07-30 08:21:15 -0300

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,4 +7,20 @@ def emotion_detector(text_to_analyze):
     texto = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json=texto, headers = headers)
-    return response.text
+    var_txt = response.text
+    var_dict = json.loads(var_txt)
+    anger_score = var_dict['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = var_dict['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = var_dict['emotionPredictions'][0]['emotion']['fear']
+    joy_score = var_dict['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = var_dict['emotionPredictions'][0]['emotion']['sadness']
+    dominant_emotion = max(var_dict['emotionPredictions'][0]['emotion'], key = var_dict['emotionPredictions'][0]['emotion'].get)
+
+    return {
+    'anger': anger_score,
+    'disgust': disgust_score,
+    'fear': fear_score,
+    'joy': joy_score,
+    'sadness': sadness_score,
+    'dominant_emotion': dominant_emotion
+    }
